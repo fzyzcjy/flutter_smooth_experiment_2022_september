461f394082fdd52f6601af8ffda75b7a252a3a8c
fzyzcjy <ch271828n@outlook.com>
2022-09-18 08:23:08 +0800

    add empty Smooth

---
+++ packages/smooth/lib/src/smooth.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Smooth extends StatelessWidget {
+  const Smooth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TODO;
+  }
+}
